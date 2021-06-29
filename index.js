5e3a858845914bc4253f5f8cc4876513beb796b7
Kyle <kj.mckinley@gmail.com>
2021-06-28 21:12:22 -0500

    added comments for clarity

--- index.js
+++ index.js
@@ -175,7 +175,7 @@ const writeToPage = (htmlContent) => {
 // generates questions
 console.log(`Welcome to the Team Profile Generator! Please follow the prompts below and create your team!`);
 
-promptUser()
+userPrompt()
     .then(data => generatePage(data))
-    .then(generateHtml => writePage(generateHtml))
+    .then(generateHtml => writeToPage(generateHtml))
     .catch(err => console.log(err));
--- src/page-template.js
+++ src/page-template.js
@@ -52,8 +52,9 @@ const addEmployee = employeeInfo => {
                         </div>
                         <div class="content">
                             <p>${newEmployee.getId()}<br />
-                            ${newEmployee.getEmail()}<br />
-                            ${extraInfo}</p>
+                            ${extraInfo}<br />
+                            ${newEmployee.getEmail()}
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -62,3 +63,40 @@ const addEmployee = employeeInfo => {
     
     return allCards;
 };
+
+// pushes user imput into the index.html file
+const generatePage = templateData => {
+    return `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>My Team Profile</title>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.1/css/bulma.min.css" />
+        </head>
+
+        <body>
+            <section class="hero is-primary is-bold">
+                <div class="hero-body">
+                    <div class="container">
+                        <h1 class="title">Company Team</h1>
+                    </div>
+                </div>
+            </section>
+
+            <main class="m-6">
+                <div class="columns is-flex is-flex-wrap-wrap is-flex-direction-row is-justify-content-center">
+                    ${addEmployee(templateData)}
+                </div>
+            </main>
+
+        </body>
+
+        </html>
+    `;
+};
+
+module.exports = generatePage;
