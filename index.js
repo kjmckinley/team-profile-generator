b4a584556c813859b6359564e0282c75722337a0
Kyle <kj.mckinley@gmail.com>
2021-06-28 19:20:52 -0500

    added an array of questions for inquirer in the index.js

--- index.js
+++ index.js
@@ -4,4 +4,142 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generatePage = require('./src/page-template.js')
 const nameFormatter = require('./utils/helper');
-const Employees = [];
+const everyEmployee = [];
+
+// An array of questions objects that will be used by inquirer
+const questions = [
+
+    // user choice
+    {
+        type: 'list',
+        name: 'role',
+        message: 'What is your new employee\'s role at the company?',
+        choices: // function to allow only a single manager to be created
+            () => {
+            if (everyEmployee.some(employee => employee.role === 'Manager')) {
+                return ['Engineer', 'Intern']    
+            } else {
+                return ['Manager', 'Engineer', 'Intern']
+            }
+        }
+    },
+
+    // new employee first name
+    {
+        type: 'input',
+        name: 'firstName',
+        message: ({ role }) => `What is the ${role.toLowerCase()}'s first name?`,
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Error: You cannot leave this field blank!');
+                return false;
+            }
+        }
+    },
+
+    // new employee last name
+    {
+        type: 'input',
+        name: 'lastName',
+        message: ({ firstName }) => `What is the ${nameFormatter(firstName)}'s last name?`,
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Error: You cannot leave this field blank!');
+                return false;
+            }
+        }
+    },
+
+    //new employee id number
+    {
+        type: 'input',
+        name: 'id',
+        message: ({ firstName }) => `What is ${nameFormatter(firstName)}'s ID number?`,
+        validate: idInput => {
+            if (!isNaN(parseInt(idInput))) {
+                return true;
+            } else {
+                console.log('Please enter a valid numeric value for the ID number!');
+                return false;
+            }
+        }
+    },
+
+    // new employee office number
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message:  ({ firstName }) => `What is ${nameFormatter(firstName)}'s office number?`,
+        when: ({ role }) => {
+            if (role === 'Manager') {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        validate: officeNumberInput => {
+            if (!isNaN(parseInt(officeNumberInput))) {
+                return true;
+            } else {
+                console.log('Please enter a valid numeric value.');
+                return false;
+            }
+        }
+    },
+
+    // engineer github username
+    {
+        type: 'input',
+        name: 'github',
+        message: ({ firstName }) => `What is ${nameFormatter(firstName)}'s GitHub userame?`,
+        when: ({ role }) => {
+            if (role === 'Engineer') {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Error: You cannot leave this field blank!');
+                return false;
+            }
+        }
+    },
+
+    // intern school name
+    {
+        type: 'input',
+        name: 'school',
+        message: ({ firstName }) => `What school does ${nameFormatter(firstName)} go to?`,
+        when: ({ role }) => {
+            if (role === 'Intern') {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        validate: schoolInput => {
+            if (schoolInput) {
+                return true;
+            } else {
+                console.log('Error: You cannot leave this field blank!');
+                return false;
+            }
+        }
+    },
+
+    // user choice to add another employee
+    {
+        type: 'confirm',
+        name: 'addEmployee',
+        message: 'Would you like to add another employee?',
+        default: true
+    }
+]
