e390af7f9df25d1ad368d07c7ce3092a6e416d68
Kyle <kj.mckinley@gmail.com>
2021-06-28 20:44:59 -0500

    created page-templates

--- src/page-template.js
+++ src/page-template.js
@@ -5,3 +5,34 @@ const Manager = require('../lib/Manager');
 const Engineer = require('../lib/Engineer');
 const Intern = require('../lib/Intern');
 const nameFormatter = require('../utils/helper');
+
+const addEmployee = employeeInfo => {
+
+    // initiates html string for employee cards
+    let allCards = '';
+
+    employeeInfo.forEach(employee => {    
+        
+        const { firstName, lastName, id, role } = employee;
+        
+        // blank variable to be filled with user input
+        let extraInfo = '';
+        let newEmployee = '';
+        
+        // fills in each case with the correct employee information
+        switch (role) {
+            case 'Manager':
+                newEmployee = new Manager(nameFormatter(firstName), nameFormatter(lastName), id, employee.officeNumber);
+                extraInfo = newEmployee.getOfficeNumber();
+                break;
+            case 'Engineer': 
+                newEmployee = new Engineer(nameFormatter(firstName), nameFormatter(lastName), id, employee.github);
+                extraInfo = newEmployee.Github();
+                break;
+            case 'Intern':
+                newEmployee = new Intern(nameFormatter(firstName), nameFormatter(lastName), id, employee.school);
+                extraInfo = newEmployee.getSchool();
+                break;
+            default:
+                newEmployee = new Employee(nameFormatter(firstName), nameFormatter(lastName), id);
+        };
